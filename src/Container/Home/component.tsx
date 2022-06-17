b344cbbd5b8bae14892cf0c36fd8820bc6301f6f
Tuan <mr.tuan1749@gmail.com>
2022-06-17 21:35:43 +0700

    update page home layout

---
+++ src/Container/Home/component.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import LayoutHome from "../../Layout/Home";
+import View from "./view";
+export default class HomeComponent extends Component {
+  render() {
+    return (
+      <LayoutHome>
+        <View />
+      </LayoutHome>
+    );
+  }
+}
--- src/Layout/Home/index.tsx
+++ src/Layout/Home/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
+import Style from "./index.module.scss";
 import Header from "../../Components/Header";
+import Footer from "../../Components/Footer";
 interface HomePageProps {
   children: React.ReactNode;
 }
 export default function HomePage({ children }: HomePageProps) {
   return (
-    <div>
+    <div className={Style["container"]}>
       <Header />
-      {children}
+      <div className={Style["bg--right"]}></div>
+      <div className={Style["content"]}>{children}</div>
+      <Footer />
     </div>
   );
 }
